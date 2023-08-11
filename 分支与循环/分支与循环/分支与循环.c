a4f00bb050923e4a874e54deae7e218e0df04aab
luoshikong <141638551+luoshikong@users.noreply.github.com>
2023-08-11 20:10:50 +0800

    Add files via upload

---
+++ 分支与循环/分支与循环/分支与循环.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int day = 0;
+	scanf_s("%d", &day);
+	switch(day)
+	{
+	case 1:
+		printf("1\n");
+	case 2:
+		printf("2\n");
+
+	}
+	//int i = 1;
+	//while (i <= 100)
+	//{
+		//if (i % 2 == 1)
+			//printf("%d\n", i);
+		//i++;
+
+	//}
+	return 0;
+}
